26c774827abd968a006cacefa2121c390a350cc6
Kiet Nguyen <Kietsu3756@gmail.com>
2021-11-06 17:27:11 -0700

    Solve https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem

---
+++ linuxShellBash/loopingAndSkipping.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# MIT License
+# 
+# Copyright (c) 2021 Kiet Nguyen (Kietsu3756)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+loopingAndSkippingWithThreeExpressForLoop(){
+    for (( i = 1; i <= 99; i = i + 2 ))
+    do
+        printf "${i}\n"
+    done
+}
+
+loopingAndSkippingWithRangeForLoopAndModulo(){
+    for i in {1..99}
+    do
+        if [ $(($i % 2)) != 0 ]; then
+            printf "${i}\n"
+        fi
+    done
+}
+
+loopingAndSkippingWithRangeForLoopWithStep(){
+    for i in {1..99..2}
+    do
+        printf "${i}\n"
+    done
+}
+
+loopingAndSkippingWithRangeForLoopWithStep
