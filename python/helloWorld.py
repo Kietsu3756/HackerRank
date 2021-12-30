cacea624a885354d8cb6da2e2a0c61e33fcf79c3
Kietsu3756 <Kietsu3756@gmail.com>
2021-12-29 20:41:08 -0500

    Solve https://www.hackerrank.com/challenges/py-hello-world/problem

---
+++ python/helloWorld.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+# Copyright (c) 2021 Kiet Nguyen (Kietsu3756)
+# 
+# This work is licensed under the terms of the MIT license.  
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+print("Hello, World!")
